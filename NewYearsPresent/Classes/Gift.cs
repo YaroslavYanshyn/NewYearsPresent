857ad0176bb0e5010c282a9c0ea7c0c8246e5112
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2018-04-26 16:03:34 +0300

    add Gift and IGift

---
+++ NewYearsPresent/Classes/Gift.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewYearsPresent.Interfaces;
+
+namespace NewYearsPresent.Classes
+{
+    public class Gift : IGift
+    {
+        private ICollection<Sweet> items;
+
+        public Gift()
+        {
+            items = new List<Sweet>();
+        }
+
+        public void Add(Sweet sweets)
+        {
+            items.Add(sweets);
+        }
+
+        public int GiftWeight()
+        {
+            return items.Sum(x => x.weight);
+        }
+
+        public void Sort()
+        {
+            var temp = items.OrderBy(x=>x.weight).ToList();
+            items.Clear();
+            foreach (var item in temp)
+            {
+                items.Add(item);
+            }
+        }
+
+        public IEnumerable<Sweet> FindCandyBySugar(int min, int max)
+        {
+            return items.Where(x => (x.sugar >= min) && (x.sugar <= max)).ToList();
+        }
+
+        public IEnumerable<Sweet> Items
+        {
+            get { return this.items; }
+        }
+    }
+}
---
+++ NewYearsPresent/Interfaces/IGift.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewYearsPresent.Classes;
+
+namespace NewYearsPresent.Interfaces
+{
+    public interface IGift
+    {
+        IEnumerable<Sweet> Items { get; }
+
+        void Add(Sweet sweets);
+
+        int GiftWeight();
+
+        void Sort();
+
+        IEnumerable<Sweet> FindCandyBySugar(int min, int max);
+    }
+}
